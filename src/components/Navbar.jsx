acd22cab5b9c2defaae1d64db468e174cabe3a3e
nandhini-11-cmd <nandhini.ks11@gmail.com>
2025-07-30 18:37:16 +0530

    margin left update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
   return (
     <nav className="bg-orange-500 text-white p-4 ">
        <div className="flex justify-between items-center">
-      <Link to="/" className="text-2xl font-bold ml-14 md:ml-140">🍽️ YummyHub</Link>
+      <Link to="/" className="text-2xl font-bold ml-18 md:ml-140">🍽️ YummyHub</Link>
      <button onClick={() => setShowMenu(!showMenu)} className="md:hidden text-blue-600 font-bold text-sm  ">
           {showMenu ? '\u2716' : '\u2630'}
         </button>
